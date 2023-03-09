a9b3915c0229e23ef05c93856bae865563aa6040
MungunshagaiiT <mungunshagai.t@pinecone.mn>
2023-03-09 15:38:01 +0800

    first commit

---
+++ src/services/fetchPopularCategory.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const FetchPopularCategory = () => {
+  const [productCategory, setProductCategory] = useState([]);
+  useEffect(() => {}, []);
+  return <div>FetchPopularCategory</div>;
+};
+
+export default FetchPopularCategory;
